8e4ef1d2c04565c2cc9d1a8dac9b83587c53af40
gonyda <117551531+gonyda@users.noreply.github.com>
2022-11-06 11:25:27 +0900

    Add files via upload

---
+++ demo/src/main/java/com/example/cache/CacheUtils.java
@@ -0,0 +1,56 @@
+package com.example.cache;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public final class CacheUtils {
+	
+	private static final Map<String, String> CACHELIST = new HashMap<>();
+	private static final Map<String, Integer> CACHEHIT = new HashMap<>();
+	
+	// 캐시 사이즈, 5개 제한
+	private static final int MAXSIZE = 5;
+	
+	// 캐시 조회
+	public static String getCache(String key) {
+		if(!CACHELIST.containsKey(key)) {
+			return null;
+		}
+		
+		CACHEHIT.put(key, CACHEHIT.get(key) + 1);
+		return CACHELIST.get(key);
+	}
+	
+	// 캐시 저장
+	public static void saveCache(String key, String value) {
+		if(isMaxSize()) {
+			String minKey = getMinValue().getKey();
+			CACHELIST.remove(minKey);
+			CACHEHIT.remove(minKey);
+		}
+		CACHELIST.put(key, value);
+		saveCacheHit(key);
+	}
+
+	// 캐시 히트 저장
+	private static void saveCacheHit(String key) {
+		CACHEHIT.put(key, 0);
+	}
+	
+	// 캐시 한도 체크
+	// true: 한도 초과
+	private static boolean isMaxSize() {
+		return CACHELIST.size() >= MAXSIZE ? true : false;
+	}
+	
+	// 최소 CACHEHIT Map 가져오기
+	private static Entry<String, Integer> getMinValue() {
+		return Collections.min(CACHEHIT.entrySet(), (v1, v2) -> v1.getValue().compareTo(v2.getValue()));
+	}
+	
+}
---
+++ demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+		Assert.isTrue(1<0, "test");
+	}
+
+}
