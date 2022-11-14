81ea95054ad82bcfe7b809207f16336992e145a8
gonyda <117551531+gonyda@users.noreply.github.com>
2022-11-14 14:14:19 +0900

    Add files via upload

---
+++ src/main/java/com/example/cache/JobCode.java
@@ -0,0 +1,23 @@
+package com.example.cache;
+
+public enum JobCode {
+	TCT("무선해지미납"),
+	TCB("유선해지미납"),
+	WIB("유선"),
+	WIT("통합"),
+	SMT("스마트폰"),
+	MVS("모비우스"),
+	TRM("해지후할부"),
+	TTM("단말해지미납");
+
+	private String jobCode;
+	
+	JobCode(String jobCode) {
+		this.jobCode = jobCode;
+	}
+	
+	public String getJobCode() {
+		return jobCode;
+		
+	}
+}
